bac673cf86a6db7b104dd356e0393948e20d1c68
mannie411 <imohyo@gmail.com>
2021-03-17 02:44:10 +0000

    change routing module

--- src/index.js
+++ src/index.js
@@ -1,38 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
 
-import './assets/css/style.css'
+import "./assets/css/style.css";
 
-import routes from './routes'
-import Header from './components/Header';
-import Footer from './components/Footer';
-
-
+import routes from "./routes";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router >
+    <Router>
       <Header />
       <div className="container">
-
         <Switch>
-          {routes.map((route, idx) =>
+          {routes.map((route, idx) => (
             <Route
               key={idx}
               path={route.path}
               exact={route.exact}
               children={<route.component />}
             />
-          )}
+          ))}
         </Switch>
-
       </div>
       <Footer />
     </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
