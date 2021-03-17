7c3ffe9a1703a5c0c02486c03eb06dbbbb68a075
mannie411 <imohyo@gmail.com>
2021-03-17 01:27:14 +0000

    added production build

---
+++ build/precache-manifest.e46a458ab668a6323c4691a5039d9969.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5bf9afa9d449cebd596c9cee8d4c169",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64009d865a432c36f1c9",
+    "url": "/static/css/main.cd2bd462.chunk.css"
+  },
+  {
+    "revision": "0720e81219da059a103c",
+    "url": "/static/js/2.cc871aab.chunk.js"
+  },
+  {
+    "revision": "f993c07f5558dd16849987606616a4cf",
+    "url": "/static/js/2.cc871aab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8b996c6d7a497260ced",
+    "url": "/static/js/3.1be5cde1.chunk.js"
+  },
+  {
+    "revision": "64009d865a432c36f1c9",
+    "url": "/static/js/main.6e8cb6ef.chunk.js"
+  },
+  {
+    "revision": "d26ad92702750ec32fd1",
+    "url": "/static/js/runtime-main.e63c17a7.js"
+  }
+]);
