35a67f484f10348829470b0339b73c1816ccf46e
mannie411 <imohyo@gmail.com>
2021-03-17 02:20:26 +0000

    fixed build repo and added github pages index

---
+++ precache-manifest.4da53791424696fac67b0b6c2465c4bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c8fcb5164bcc380734a0826d8123602",
+    "url": "/happyapp/index.html"
+  },
+  {
+    "revision": "64009d865a432c36f1c9",
+    "url": "/happyapp/static/css/main.cd2bd462.chunk.css"
+  },
+  {
+    "revision": "0720e81219da059a103c",
+    "url": "/happyapp/static/js/2.cc871aab.chunk.js"
+  },
+  {
+    "revision": "f993c07f5558dd16849987606616a4cf",
+    "url": "/happyapp/static/js/2.cc871aab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8b996c6d7a497260ced",
+    "url": "/happyapp/static/js/3.1be5cde1.chunk.js"
+  },
+  {
+    "revision": "64009d865a432c36f1c9",
+    "url": "/happyapp/static/js/main.6e8cb6ef.chunk.js"
+  },
+  {
+    "revision": "119c98255576368740ea",
+    "url": "/happyapp/static/js/runtime-main.ab752e0e.js"
+  }
+]);
